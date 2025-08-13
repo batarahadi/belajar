b519b283e0e011ffb8132439e37d4df0f07defc4
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-08-14 05:23:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pandas as pd
 dataMenu = {"listMenu" : ["Nasi Goreng", "Mie Goreng", "Ayam Penyet", "Sate Ayam", "Gado-Gado"],
             "price" : [15000, 12000, 20000, 18000, 10000]}
 
+# membuat DataFrame
+dfMenu = pd.DataFrame(dataMenu)
 
-df = pd.DataFrame(dataMenu)
-print(df)
+# Format harga satuan dengan titik dan menambahkan "Rp"
+dfMenu["price"] = dfMenu["price"].apply(lambda x: f"Rp {x:,}".replace(",", "."))
+
+# Menampilkan DataFrame
+print(dfMenu)
